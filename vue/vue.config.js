2f580e0c69f887810adc786467d65f59de2d4192
Alexey Kuznetsov <noofsaeidh@outlook.com>
2019-01-09 22:23:36 +0300

    add abp back
    to use declared types just use: "import '@/lib/abp';" and it will have intellisense

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,6 +1,25 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
-    baseUrl: '/',
-    configureWebpack: {
-        devtool: 'source-map'
-    }
-}
+  baseUrl: '/',
+  configureWebpack: config => {
+    return {
+      devtool: 'source-map',
+
+      plugins: [
+        new CopyWebpackPlugin([
+          {
+            //todo: perhaps should add if use signal r
+            //       from:'node_modules/@aspnet/signalr/dist/browser/signalr.min.js',
+            //       to:'dist'
+            //   },{
+            //       from:'node_modules/abp-web-resources/Abp/Framework/scripts/libs/abp.signalr-client.js',
+            //       to:'dist'
+            //   },{
+            from: 'src/lib/abp.js',
+            to: 'dist',
+          },
+        ]),
+      ],
+    };
+  },
+};
