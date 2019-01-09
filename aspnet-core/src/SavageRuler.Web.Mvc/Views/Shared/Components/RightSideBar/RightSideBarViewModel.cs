e591266c061f218f6ffd480036807921624c1be1
Alexey Kuznetsov <noofsaeidh@outlook.com>
2019-01-09 23:24:01 +0300

    rm reduntand projects

--- aspnet-core/src/SavageRuler.Web.Mvc/Views/Shared/Components/RightSideBar/RightSideBarViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using SavageRuler.Configuration.Ui;
-
-namespace SavageRuler.Web.Views.Shared.Components.RightSideBar
-{
-    public class RightSideBarViewModel
-    {
-        public UiThemeInfo CurrentTheme { get; set; }
-    }
-}
