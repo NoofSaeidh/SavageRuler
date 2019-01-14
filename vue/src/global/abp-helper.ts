5da61375080d9d8f2351ccbfad5bd1f3e6040d63
Alexey Kuznetsov <noofsaeidh@outlook.com>
2019-01-14 21:39:55 +0300

    exclude abp helpers (cannot compile because of changed $store)

--- vue/src/global/abp-helper.ts
+++ vue/src/global/abp-helper.ts
@@ -1,30 +1,30 @@
-import { AppConsts } from '@/global/app-consts';
-import abpUtils from '@/lib/abp-utils';
+// import { AppConsts } from '@/global/app-consts';
+// import abpUtils from '@/lib/abp-utils';
 
-export class AbpHelper {
-  loadAbpScripts() {
-    this.safeLoadScript(AppConsts.getAppScriptsUrlFull);
-  }
+// export class AbpHelper {
+//   loadAbpScripts() {
+//     this.safeLoadScript(AppConsts.getAppScriptsUrlFull);
+//   }
 
-  // the same as loadScript in abp
-  // but check if it already loaded don't load
-  // todo: perhaps should add refresh in such case
-  safeLoadScript(url: string) {
-    if (!this.scriptIsLoaded(url)) {
-      abpUtils.loadScript(url);
-    }
-  }
+//   // the same as loadScript in abp
+//   // but check if it already loaded don't load
+//   // todo: perhaps should add refresh in such case
+//   safeLoadScript(url: string) {
+//     if (!this.scriptIsLoaded(url)) {
+//       abpUtils.loadScript(url);
+//     }
+//   }
 
-  scriptIsLoaded(url: string): boolean {
-    const scripts = document.getElementsByTagName('script');
-    for (const script of scripts) {
-      if (script.src === url) {
-        return true;
-      }
-    }
-    return false;
-  }
-}
+//   scriptIsLoaded(url: string): boolean {
+//     const scripts = document.getElementsByTagName('script');
+//     for (const script of scripts) {
+//       if (script.src === url) {
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
+// }
 
-const instance = new AbpHelper();
-export default instance;
+// const instance = new AbpHelper();
+// export default instance;
