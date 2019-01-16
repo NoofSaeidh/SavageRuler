a9b08a54ae5c415331358ec97a1025d104ca066f
Alexey Kuznetsov <noofsaeidh@outlook.com>
2019-01-17 02:38:39 +0300

    working on screen navigation

--- vue/src/components/ReadOnlyScreen.vue
+++ vue/src/components/ReadOnlyScreen.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div @keydown="arrowPressed">
     <!-- modal dialog -->
     <ReadOnlyModal v-if="selected" :show="showModal" @hide="showModal=false" :title="title">
       <ReadOnlyForm :descriptor="objectDescriptor" :item="selected"></ReadOnlyForm>
@@ -14,7 +14,12 @@
     <div v-else>
       <!-- single item -->
       <div v-if="showTable">
-        <ShortTable v-if="items" :items="items" :descriptor="objectDescriptor" :onRowClicked="rowClicked"></ShortTable>
+        <ShortTable
+          v-if="items"
+          :items="items"
+          :descriptor="objectDescriptor"
+          :onRowClicked="rowClicked"
+        ></ShortTable>
       </div>
 
       <!-- grid -->
@@ -51,6 +56,7 @@ export default class ReadOnlyScreen<T extends Entity<TKey>, TKey> extends Vue {
   @Prop() objectDescriptor!: ViewObjectDescriptor<T>;
 
   selected: T | null = null;
+  selectedIndex: number | null = null;
   items: T[] | null = null;
   title: string | null = null;
   loading: boolean = true;
@@ -97,6 +103,7 @@ export default class ReadOnlyScreen<T extends Entity<TKey>, TKey> extends Vue {
 
   rowClicked(record: T, index: number, event: MouseEvent) {
     this.selected = record;
+    this.selectedIndex = index;
     this.title = record[this.objectDescriptor.titleKey];
     // show selected item in modal on click
     // or as common screen when click with alt
@@ -119,6 +126,60 @@ export default class ReadOnlyScreen<T extends Entity<TKey>, TKey> extends Vue {
 
   @Watch('$route') async routeChanged(newRoute: Route, oldRoute: Route) {
     await this.load(newRoute);
+  }
+
+  async arrowPressed(event: KeyboardEvent) {
+    if (event.keyCode < 37 || event.keyCode > 40) {
+      return;
+    }
+    let index = this.selectedIndex || 1;
+    switch (event.keyCode) {
+      // swipe to the side
+      case 37 /* left */:
+        if (!this.showModal) {
+          this.showTable = false;
+        }
+        if (index > 1) {
+          index--;
+        }
+        break;
+
+      case 39 /* right */:
+        if (!this.showModal) {
+          this.showTable = false;
+        }
+        // todo: check max index
+        index++;
+        break;
+
+      // // show table
+      // case 38 /* up */:
+      //   if (this.showModal) {
+      //     this.showModal = false;
+      //   }
+      //   if (!this.showTable) {
+      //     this.showTable = true;
+      //   }
+
+      //   break;
+
+      // // show single element
+      // case 40 /* down */:
+      //   if (!this.showModal) {
+      //     this.showTable = false;
+      //   }
+      //   break;
+    }
+
+    if (this.items == null) {
+      await this.fetchItems();
+    }
+    if (index >= this.items!.length) {
+      index = this.items!.length - 1;
+    }
+
+    this.selectedIndex = index;
+    this.selected = this.items![index];
   }
 }
 </script>
