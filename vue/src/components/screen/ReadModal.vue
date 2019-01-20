6cc633246055e789b9d3bab7dfe2df6cce1189a7
Alexey Kuznetsov <noofsaeidh@outlook.com>
2019-01-20 20:27:48 +0300

    refactor modal

--- vue/src/components/screen/ReadModal.vue
+++ vue/src/components/screen/ReadModal.vue
@@ -1,19 +1,14 @@
 <template>
   <div>
     <b-modal
-      :ref="refId"
+      v-model="showModal"
       ok-only
-      @hide="hide"
       header-class="text-center d-block"
       hide-header
       hide-footer
       centered
       size="lg"
     >
-      <!-- <div v-if="title" slot="modal-title" class="modal-title text-center d-block">
-        <b v-text="title"></b>
-      </div> -->
-
       <slot></slot>
     </b-modal>
   </div>
@@ -25,27 +20,21 @@ import { Modal } from 'bootstrap-vue';
 
 @Component({})
 export default class ReadModal extends Vue {
-  private static $uniqueId: number = 0;
-
   @Prop() show!: boolean;
-  @Prop() title?: string;
 
-  refId = 'modalReadOnlyForm_' + ReadModal.$uniqueId++;
+  private $_show = false;
 
-  $refs!: {
-    [index: string]: Modal,
-  };
-
-  mounted() {
-    this.showChanged(this.show);
+  created() {
+    this.$_show = this.show;
   }
 
-  @Watch('show') showChanged(newValue: boolean) {
-    if (newValue) {
-      this.$refs[this.refId].show();
-    }
-    else {
-      this.$refs[this.refId].hide();
+  get showModal() {
+    return this.$_show;
+  }
+  set showModal(value: boolean) {
+    this.$_show = value;
+    if (!this.$_show) {
+      this.hide();
     }
   }
 
