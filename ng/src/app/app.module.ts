72237d59e3071e9e3637c26ebe462e1c4258773e
Alexey Kuznetsov <noofsaeidh@outlook.com>
2019-01-24 00:54:43 +0300

    rm bootstrap (on of)

--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +8,6 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    NgbModule,
     BrowserModule,
     AppRoutingModule,
     MDBBootstrapModule.forRoot(),
