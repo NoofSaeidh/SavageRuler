cc2172a5f4e19bb668b5d102352587e2d1c11e4d
Alexey Kuznetsov <noofsaeidh@outlook.com>
2019-01-30 00:09:11 +0300

    fix tests

--- ng/src/app/api/services/api-crud.service.spec.ts
+++ ng/src/app/api/services/api-crud.service.spec.ts
@@ -1,10 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
 import { ApiCrudService } from './api-crud.service';
 import { Power } from 'src/app/views/powers/types/power';
+import { ApiDescriptor } from '../types/api-descriptor';
+
+
 
 describe('ApiCrudService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [
+      {provide: ApiDescriptor, useValue: new ApiDescriptor({})}
+    ]
+  }));
 
   it('should be created', () => {
     const service: ApiCrudService<Power, number> = TestBed.get(ApiCrudService);
